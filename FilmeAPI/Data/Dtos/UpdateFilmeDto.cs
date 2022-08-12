138e67c7c09c9b279d218e549c4cbd561fc4115f
Claudia Sousa <claudiasousacosta2966@gmail.com>
2022-08-12 16:09:44 -0300

    FilmeAPI

---
+++ FilmeAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+
+        [StringLength(30, ErrorMessage = "O genero passar de 30 caracteres")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duração deve ter no minimo  1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
+
---
+++ FilmeAPI/Profiles/FilmeProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using FilmeAPI.Data.Dtos;
+using FilmeAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+          public FilmeProfile()
+        {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<Filme, ReadFilmeDto>();
+            CreateMap<UpdateFilmeDto, Filme>();
+        }
+    }
+}
